8f95d9bdab74eb0560d045f3050e76412e198a98
Henzo <henzofelipe1@gmail.com>
2023-11-18 20:56:33 -0300

    Translated comments to PT-BR

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
 import pygame
 import sys
+
+# Adiciona a pasta /src/ pro PYTHONPATH
 sys.path.append("src/")
+
+# Importa todo o pacote de src/
 from src import *
 
-print(dir(menu))
 
-# Sets all the Paths
+# Define caminho de todas as pastas que usaremos
 ASSET_PATH = "assets/"
-TEXTURE_PATH = "assets/textures/"
+TEXTURE_PATH = ASSET_PATH + "textures/"
 MUSIC_PATH = ASSET_PATH + "music/"
 SOUNDS_PATH = ASSET_PATH + "sounds/"
 FONTS_PATH = ASSET_PATH + "fonts/"
 
-# Screen default size
+# Tamanho da tela
 WIDTH = 1280
 HEIGHT = 720
 
-# Initializes pygame
+# Inicializa
 pygame.init()
 
-# Defines the displayer configs    
+# Cria algumas configurações do display   
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Vectrun")
 pygame.display.set_icon(pygame.image.load(TEXTURE_PATH + "icon.png"))
 
-# Creates internal fps clock
+# Cria o relógio interno do FPS
 fps_clock = pygame.time.Clock()
 
-# Initializes everything
+# Aqui vai ser criado todos os objetos (jogadores, deck, menus)
 menu_1 = menu.Grid_Game(TEXTURE_PATH + "grid.png", (WIDTH / 2 - 300, 0), 600, 600)
 
-# Game's loop
+# Loop do jogo
 while True:
+    # Loop dos eventos principal (mexeremos no futuro)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    # Aqui vão ser atualizados todos objetos (cada um com seu update())
     menu_1.update()
+
+
+    # Aqui vão ser mostrado tudo o que foi atualizado
     screen.blit(menu_1._image, menu_1._rect)
 
-    # Updates the displayer
+
+    # Enfim atualiza o display
     pygame.display.update()
     fps_clock.tick(30)
--- src/menu.py
+++ src/menu.py
@@ -2,14 +2,14 @@ import pygame
 from entity import *
 
 class Menu(Entity):
-    def __init__(self, image_path, x_y, WIDTH, HEIGHT):
+    def __init__(self, image_path, x_y, width, height):
         super().__init__(image_path, x_y)
 
-        # Transforms it so it fills the screen
-        self._image = pygame.transform.scale(self._image, (WIDTH, HEIGHT))
+        # Transforma a imagem pro tamanho (width, height)
+        self._image = pygame.transform.scale(self._image, (width, height))
 
 class Grid_Game(Menu):
-        def __init__(self, image_path, x_y, WIDTH, HEIGHT):
-            super().__init__(image_path, x_y, WIDTH, HEIGHT)
+        def __init__(self, image_path, x_y, width, height):
+            super().__init__(image_path, x_y, width, height)
         
         
