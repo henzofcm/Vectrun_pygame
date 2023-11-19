3cc6bb9a2ad81ca4eae3f9fab578cb170881510e
Henzo <henzofelipe1@gmail.com>
2023-11-19 19:25:03 -0300

    Smoothed the scale transform

--- src/entity.py
+++ src/entity.py
@@ -6,7 +6,7 @@ class Entity(pygame.sprite.Sprite, metaclass=ABCMeta):
     def __init__(self, image_path, x_y, scale_size):
         super().__init__()
         self.image = pygame.image.load(image_path).convert_alpha()
-        self.image = pygame.transform.scale(self.image, scale_size)
+        self.image = pygame.transform.smoothscale(self.image, scale_size)
         
         self.rect = self.image.get_rect(topleft=x_y)
 
