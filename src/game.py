04d370d8d7e529fb8a0ffc30d3463d417efbc91d
Henzo <henzofelipe1@gmail.com>
2023-12-06 21:48:46 -0300

    Merged players and bots movement functions to be more abstract

--- src/game.py
+++ src/game.py
@@ -12,6 +12,11 @@ class Grid_Game(Entity):
     def __init__(self, image_path, x_y, scale_size, bot_number):
         super().__init__(image_path, x_y, scale_size)
 
+        # Atributos para o estado do jogo
+        self.next_menu = "none"
+        self._game_turn = 0
+        self._mov_stage = -1
+
         # Timer interno e holders pra quando o jogador clicar na carta
         self._timer = 0
         self._clicked = False
@@ -29,17 +34,12 @@ class Grid_Game(Entity):
         __bot_list = []
 
         for bot in range(bot_number):
-            __bot_list.append(Player(bot + 2, (GRID_X / 2 - 45, GRID_Y / 2 - 45), (RIDER_X, RIDER_Y), self._deck))
+            __bot_list.append(Bot(bot + 2, (GRID_X / 2 - 45, GRID_Y / 2 - 45), (RIDER_X, RIDER_Y), self._deck))
 
         self._bots = pygame.sprite.Group(__bot_list)
 
         # Grupo com todos personagens animados (bots e player)
-        self._all_riders = pygame.sprite.Group(self._bots.sprites(), (self._player.sprite))
-
-        # Menu para onde o jogador decidir ir
-        self.next_menu = "none"
-        self._turn = 0
-        self._turn_stage = 0
+        self._all_riders = pygame.sprite.Group((self._player.sprite), self._bots.sprites())
 
     def update(self, screen):
         # Eventos principais deste menu
@@ -55,31 +55,26 @@ class Grid_Game(Entity):
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1 and not self._clicked:
-                    self.__card_clicked()
+                    self.__validate_click()
 
         # Desenha o tabuleiro no layer mais baixo
         screen.blit(self.image, self.rect)
 
-        # Desenha o contorno das cartas
+        # Desenha o contorno e as cartas
         if self._player.sprite:
             self.choice_preview(screen)
-
-        # Se tiver clicado, roda o movimento do jogador
-        if self._clicked and self._player.sprite:
-            self.move_player(self._player.sprite)
-
-        # Desenha as linhas dos bots
-        for bot in self._bots.sprites():
-            bot.line = pygame.draw.lines(screen, bot._color, False, bot._path + [bot.rect.center], width=6)
-
-        # Desenha as linhas do jogador e suas cartas
-        if self._player.sprite:
-            self._player.sprite.line = pygame.draw.lines(screen, self._player.sprite._color, False, self._player.sprite._path + [self._player.sprite.rect.center], width=6)
-
             self._player.sprite._hand.draw(screen)
 
+        # Se tiver clicado, roda o movimento do jogador ou dos bots
+        if self._clicked and self._all_riders:
+            self.move_player(self._all_riders.sprites()[self._mov_stage])
+
+        # Desenha as linhas dos riders
+        for rider in self._all_riders.sprites():
+            rider.line = pygame.draw.lines(screen, rider._color, False, rider._path + [rider.rect.center], width=6)
+
         # Verifica colisões
-        if self._player or self._bots:
+        if self._all_riders:
             self.check_collision()
 
         return False
@@ -118,17 +113,23 @@ class Grid_Game(Entity):
         # Desenha a linha
         pygame.draw.line(screen, self._player.sprite._color, start, end, width = 4)
 
+    def __validate_click(self):
+        # Verifica em qual carta clicou
+        player_card = self.__card_clicked()
+
+        # Se tiver clicado, prepara o movimento do player
+        if player_card:
+            self._clicked = True
+            self.__next_player_movement(player_card)
+
     def __card_clicked(self):
         # Se o jogador clicar na carta, _clicked = True
         for card in self._player.sprite._hand.sprites():
             if card.update():
-                self._clicked = True
-                self._clicked_card = card
-
-                # CÓDIGO RUIM, MUDAR PRO PLAYER DEPOIS (ou não)
-                self._player_target = self._player.sprite._path[-1]
-                self._player_target = (card.value[0] * DISTANCE + self._player_target[0], -card.value[1] * DISTANCE + self._player_target[1])
-
+                return card
+            
+        return None
+            
     def move_player(self, rider):
         # Atualiza as variáveis __temp_player
         self.__set_temp_variables(rider)
@@ -148,38 +149,61 @@ class Grid_Game(Entity):
         # Se ficou parado, reseta o movimento
         else:
             self.__reset_player_movement(rider)
+            self.__next_player_movement()
 
     def __set_temp_variables(self, rider):
-            # Variáveis temporárias para não duplicar o código depois
-            self.__temp_player_center = rider.rect.center
-            self.__temp_player_target = self._player_target
+        # Variáveis temporárias para não duplicar o código depois
+        self.__temp_player_center = rider.rect.center
+        self.__temp_player_target = self._player_target
 
-            # Dependendo do valor da carta, muda a coordenada relativa
-            if self._clicked_card.value[0] < 0:
-                self.__temp_player_center = (-self.__temp_player_center[0], self.__temp_player_center[1])
-                self.__temp_player_target = (-self.__temp_player_target[0], self.__temp_player_target[1])
+        # Dependendo do valor da carta, muda a coordenada relativa
+        if self._clicked_card.value[0] < 0:
+            self.__temp_player_center = (-self.__temp_player_center[0], self.__temp_player_center[1])
+            self.__temp_player_target = (-self.__temp_player_target[0], self.__temp_player_target[1])
 
-            if self._clicked_card.value[1] > 0:
-                self.__temp_player_center = (self.__temp_player_center[0], -self.__temp_player_center[1])
-                self.__temp_player_target = (self.__temp_player_target[0], -self.__temp_player_target[1])
+        if self._clicked_card.value[1] > 0:
+            self.__temp_player_center = (self.__temp_player_center[0], -self.__temp_player_center[1])
+            self.__temp_player_target = (self.__temp_player_target[0], -self.__temp_player_target[1])
 
     def __reset_player_movement(self, rider):
-         # Retorna _timer e _clicked pra 0
+        # Retorna _timer para 0
         self._timer = 0
-        self._clicked = False
 
-        # Pesca uma nova carta e adiciona à mão do player
+        # Pesca uma nova carta e adiciona à mão do rider
         card = self._deck.draw_card()
         card.rect.topleft = self._clicked_card.rect.topleft
 
         rider._hand.add(card)
 
-        # Remove a carta usada do player e limpa _clicked_card
+        # Remove a carta usada do rider e limpa _clicked_card
         rider._hand.remove(self._clicked_card)
         self._clicked_card = None
 
         # Salva a posição final do jogador no seu _path
         rider._path.append(rider.rect.center)
+
+    def __next_player_movement(self, card=None):
+        self._mov_stage += 1
+
+        # Se todos jogadores tiverem se movimentado, acaba o turno
+        if self._mov_stage == len(self._all_riders):
+            self._game_turn += 1
+            self._mov_stage = -1
+            self._clicked = False
+
+            return
+
+        # Caso contrário, prepara o jogo para rodar mais uma animação
+        next_player = self._all_riders.sprites()[self._mov_stage]
+
+        # Se não tiver passado uma carta, faz o rider escolher (em geral um bot)
+        if not card:
+            card = next_player.choose_card(self._all_riders)
+
+        self._clicked_card = card
+
+        self._player_target = next_player._path[-1]
+        self._player_target = (card.value[0] * DISTANCE + self._player_target[0], -card.value[1] * DISTANCE + self._player_target[1])
 
     def check_collision(self):
         # Laceia todos jogadores
