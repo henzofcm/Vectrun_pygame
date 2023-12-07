29374b6c0918210027308fe34665c72acf66530f
Beatriz-mirandab <mirandabeatriz1508@gmail.com>
2023-12-07 20:34:24 -0300

    add dashed line

--- src/game.py
+++ src/game.py
@@ -105,12 +105,18 @@ class Grid_Game(Entity):
 
     def __preview_selected_path(self, card, screen):
         # Pega o ponto inicial e final da reta
-        start = self._player.sprite._path[-1]
-        end = (start[0] + card.value[0] * DISTANCE, start[1] - card.value[1] * DISTANCE)
+        start = pygame.Vector2(self._player.sprite._path[-1])  # Converte para Vector2
+        card_value = pygame.Vector2(card.value[0], -card.value[1])
+        size_segments = 5  # Defina a distância desejada entre os segmentos da linha tracejada
+        num_segments = int(start.distance_to(start + card_value * DISTANCE) / size_segments)
 
-        # Desenha a linha
-        pygame.draw.line(screen, self._player.sprite._color, start, end, width = 4)
-
+        # Desenha a linha tracejada
+        for i in range(num_segments):
+            if i % 2 == 0:
+                segment_start = start + card_value * i * DISTANCE / num_segments
+                segment_end = start + card_value * (i + 1) * DISTANCE / num_segments
+                pygame.draw.line(screen, self._player.sprite._color, segment_start, segment_end, width=4)     
+        
     def __card_clicked(self):
         # Se o jogador clicar na carta, _clicked = True
         for card in self._player.sprite._hand.sprites():
